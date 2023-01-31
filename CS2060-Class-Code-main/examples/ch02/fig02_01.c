65b59705c92f356f1e1a7f8706ec627e083b6d42
VeniIK <122912638+VeniIK@users.noreply.github.com>
2023-01-30 20:59:54 -0700

    Update fig02_01.c

--- CS2060-Class-Code-main/examples/ch02/fig02_01.c
+++ CS2060-Class-Code-main/examples/ch02/fig02_01.c
@@ -1,12 +1,40 @@
-// Fig. 2.1: fig02_01.c
-// A first program in C 
+/*
+* Ian A Kras
+* M/W
+* CS2060 
+* due: 1/30/23
+* desc: This project will prompt and read in from the user: length and width. The function "calculate area" will calculate the area
+*/
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 
-// function main begins program execution 
+int calculateArea(int,int,int);
 int main( void )
 {
-   printf( "Welcome to C!\n" );
+	int area = 0;
+	int length, width;
+
+	puts("Enter value for Length: ");
+	scanf("%d", &length);
+
+	puts("Enter values for Width: ");
+	scanf("%d", &width);
+
+	// send value to function and calculate area
+	area = calculateArea(length, width, area);
+
+	printf("The Area is: %d", area );
+	return 0;
 } // end function main 
+
+int calculateArea(int length, int width, int area) {
+
+	area = length * width; 
+
+	return area;
+
+}
 
 
 
